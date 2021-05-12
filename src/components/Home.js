5d05aa13c0356532a6b74214641ca756528a7787
DeLaCruz26 <dametrik.fick@gmail.com>
2021-05-12 00:03:07 -0400

    finished home component, added links styled as buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../App.css'
 
 export default function Home() {
     return (
         <div className='showcase'>
-            <h3>Device Repair Specialist</h3>
+            <h2>Device Repair Specialist</h2>
             <h1>Schedule your repair today</h1>
+            <div className='showcase__buttons'>
+                <Link to='/services' className='showcase__btn-services'>SERVICES</Link>
+                <Link to='/schedule' className='showcase__btn-schedule'>SCHEDULE</Link>
+            </div>
         </div>
     )
 }
