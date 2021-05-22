6501c603ef15e47541d2e7d8e70af6f1b45f4080
DeLaCruz26 <dametrik.fick@gmail.com>
2021-05-22 02:25:52 -0400

    completed styled component form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react'
+import { GlobalStyle, StyledButton, StyledError, StyledForm, StyledFormWrapper, StyledInput, StyledTextArea } from './Styles'
 
 
 export default function Form() {
     
     return (
-        <div>
-       
-        </div>
+       <>
+        <GlobalStyle />
+        <StyledFormWrapper>
+            <StyledForm>
+                <h2>Contact Form</h2>
+                <br />
+                <label htmlFor='name'>Name</label>
+                <StyledInput type='text' name='name' />
+                <label htmlFor='phone'>Phone Number</label>
+                <StyledInput type='tel' name='phone' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' placeholder='Format: 123-456-7890' required />
+                <label htmlFor='message'>Message</label>
+                <StyledTextArea name='message' />
+                <StyledError>Error Message</StyledError>
+                <StyledButton type='submit'>Submit</StyledButton>
+            </StyledForm>
+        </StyledFormWrapper>
+       </>
     )
 }
