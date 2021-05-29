37183f3042cdddf9eb5302dee83da23932baf67b
DeLaCruz26 <dametrik.fick@gmail.com>
2021-05-29 01:25:48 -0400

    added data to device cards

--- src/components/servicesCards/CardStyles.js
+++ src/components/servicesCards/CardStyles.js
@@ -9,7 +9,7 @@ export const Container = styled.div`
   background-color: #fff;
   background-clip: border-box;
   border: 1px solid rgba(0, 0, 0, 0.125);
-  border-radius: 0.25rem;
+  border-radius: 0.75rem;
   box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
   overflow: hidden;
 `;
@@ -19,27 +19,30 @@ export const Body = styled.div`
   flex-direction: column;
   flex: 1 1 auto;
   min-height: 1px;
-  padding: 1.25rem;
+  padding: 0.5rem;
 `;
 
 export const Title = styled.h1`
   font-size: 1.25rem;
   font-weight: bold;
   margin: 0;
+  padding-bottom: 5px;
 `;
 
 export const Text = styled.p`
   line-height: 25px;
+  font-size: 0.75rem;
   color: #6c757d;
 `;
 
 export const Image = styled.img`
   width: 100%;
+  height: 55%;
 `;
 
 export const Button = styled.button`
   cursor: pointer;
-  margin-top: auto;
+  margin-top: 15px;
   width: 100%;
   color: #fff;
   background-color: #28a745;
@@ -50,7 +53,7 @@ export const Button = styled.button`
   vertical-align: middle;
   user-select: none;
   border: 1px solid transparent;
-  padding: 0.375rem 0.75rem;
+  padding: 0.375rem;
   font-size: 1rem;
   line-height: 1.5;
   border-radius: 0.25rem;
